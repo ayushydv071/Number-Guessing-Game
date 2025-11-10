2a4e6ec35624fcb1cbc21146a5e26f41397054dd
Ayush Yadav <0241csds239@niet.co.in>
2025-11-11 01:00:06 +0530

    Update number_guessing_game.cpp

--- number_guessing_game.cpp
+++ number_guessing_game.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 #include <cstdlib>  
 #include <ctime>
+#include <string>
 using namespace std;
+
+// Simple ANSI color helpers (optional)
+const string RESET = "\x1b[0m";
+const string BOLD = "\x1b[1m";
+const string GREEN = "\x1b[32m";
+const string CYAN = "\x1b[36m";
+
+void printHeader() {
+    cout << CYAN << BOLD;
+    cout << "  #############################################\n";
+    cout << "  #                                           #\n";
+    cout << "  #        THE GREAT GUESSING ADVENTURE       #\n";
+    cout << "  #                                           #\n";
+    cout << "  #############################################\n" << RESET;
+}
+
+void printTitleBig() {
+    cout << GREEN << BOLD;
+    cout << "\n  ____  _   _  _   _  _____  ____  _____  _   _  \n";
+    cout << " |  _ \\| | | || \\ | || ____||  _ \\| ____|| \\ | | \n";
+    cout << " | | | | | | ||  \\| ||  _|  | | | |  _|  |  \\| | \n";
+    cout << " | |_| | |_| || |\\  || |___ | |_| | |___ | |\\  | \n";
+    cout << " |____/ \\___/ |_| \\_||_____||____/|_____||_| \\_| \n" << RESET << "\n\n";
+}
 
 int main() {
     srand(time(0));
+    // Print a nicer header/title
+    printHeader();
+    printTitleBig();
     // Generate a random number between 1 and 100
     int secretNumber = rand() % 100 + 1;
     int guess;
